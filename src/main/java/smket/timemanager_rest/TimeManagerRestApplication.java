71076a3925aeabda87243a060b297d3fea084d61
Mario <mario.mai@stud.uni-due.de>
2024-07-21 19:04:22 +0200

    first commit

---
+++ src/main/java/smket/timemanager_rest/TimeManagerRestApplication.java
@@ -0,0 +1,13 @@
+package smket.timemanager_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeManagerRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeManagerRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/smket/timemanager_rest/TimeManagerRestApplicationTests.java
@@ -0,0 +1,13 @@
+package smket.timemanager_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeManagerRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
